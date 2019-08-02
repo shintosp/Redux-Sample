d38064bc8573fe0aac256c0111937cc902d1a3cd
shintosp <50572105+shintosp@users.noreply.github.com>
2019-08-02 20:04:45 +0530

    git instructions file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { updateUser, apiRequest } from "./actions/user-actions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {}, this.props.onApiRequest());
+  }
+
+  onUpdateUser = event => {
+    this.props.onUpdateUser(event.target.value);
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+        <input onChange={this.onUpdateUser} />
+        {this.props.user}
+      </div>
+    );
+  }
 }
+const mapStateToProps = (state, props) => ({
+  products: state.products,
+  user: state.user,
+  uesrPlusProp: `${state.user} ${props.aRandomProps}`
+});
 
-export default App;
+const mapActionsToProps = {
+  onUpdateUser: updateUser,
+  onApiRequest: apiRequest
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import thunk from "redux-thunk";
+import { applyMiddleware, compose, combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+import productReducer from "./reducers/product-reducer";
+import userReducer from "./reducers/user-reducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const allReducers = combineReducers({
+  products: productReducer,
+  user: userReducer
+});
+
+const allStoreEnhancers = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension && window.devToolsExtension()
+);
+
+// const allReducerEnhancers = compose(
+//   window.devToolsExtension && window.devToolsExtension()
+// );
+
+const store = createStore(
+  allReducers,
+  {
+    products: [{ name: "iphone" }],
+    user: "Michael"
+  },
+  allStoreEnhancers
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App aRandomProps="whatever" />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
